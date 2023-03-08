98ff5a9ca362987a5d42294960d4b883b0080650
Sam Hamamji <samhamamji0@gmail.com>
2023-03-09 01:48:45 +0200

    Setup pre-commit hooks

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-interface Student {
-  id: number,
-  name: string,
-  GPA?: number,
-}
-
-const sam: Student = { id: 2022, name: 'Sam', GPA: 1000 };
-
-const students: Student[] = [sam];
-
-console.log(students);
+console.log('Hello world');
