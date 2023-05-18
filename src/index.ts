d385a6279fe303a72027c2c92d910a4c8d028eba
Sam Hamamji <samhamamji0@gmail.com>
2023-05-18 23:01:14 +0300

    Omit port in validation url while in production

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import config from './config';
 import app from './api';
 
 app.listen(config.SERVER_PORT, () => {
-  console.log(`Listening at http://${config.SERVER_HOST}:${config.SERVER_PORT}`);
+  console.log(`Listening at ${config.SERVER_HOST}:${config.SERVER_PORT}`);
 });
